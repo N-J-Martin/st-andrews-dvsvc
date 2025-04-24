5c90ce67fa841da6936bff51aa290ef4d60046cf
Nadine Martin <nm275@st-andrews.ac.uk>
2025-04-24 22:17:45 +0100

    exrtact charity numbers properly

--- llm_out_db/insert_llm_output_into_DB.py
+++ llm_out_db/insert_llm_output_into_DB.py
@@ -1,23 +1,37 @@
 #import psycopg2
 import pandas as pd
+import ast
+
 #from llm_setup import get_db_logger, connect, accessors
 
 if __name__ == "__main__":
-   # LOGGER = get_db_logger()
-   # conn = connect.connect()
+#    LOGGER = get_db_logger()
+#    conn = connect.connect()
   
     #accessors.insert_charity(conn,  "www.test.com", "test", "test" )
     # need to clean up csv file - have to merge corrected with original, so all values are correct. corrected columns only contain corrections where necessary
-    #df = pd.read_csv("extracted_data.csv", usecols=["url_corrected","charity_numbers_corrected","summary_corrected","phone_corrected","email_corrected","locations_corrected","name_corrected"])
     df = pd.read_csv("extracted_data.csv", usecols=["url", "charity_numbers", "summary", "phone", "email", "locations", "name", "url_corrected","charity_numbers_corrected","summary_corrected","phone_corrected","email_corrected","locations_corrected","name_corrected"] )
-    df["url_corrected"].fillna(df["url"], inplace = True)
-    df["charity_numbers_corrected"].fillna(df["charity_numbers"], inplace = True)
-    df["summary_corrected"].fillna(df["summary"], inplace = True)
-    df["phone_corrected"].fillna(df["phone"], inplace = True)
-    df["email_corrected"].fillna(df["email"], inplace = True)
-    df["locations_corrected"].fillna(df["locations"], inplace = True)
-    df["name_corrected"].fillna(df["name"], inplace = True)
+    df["url_corrected"] = df["url_corrected"].fillna(df["url"])
+    df["charity_numbers_corrected"] = df["charity_numbers_corrected"].fillna(df["charity_numbers"])
+    df["summary_corrected"] = df["summary_corrected"].fillna(df["summary"])
+    df["phone_corrected"] = df["phone_corrected"].fillna(df["phone"])
+    df["email_corrected"] = df["email_corrected"].fillna(df["email"])
+    df["locations_corrected"] = df["locations_corrected"].fillna(df["locations"])
+    df["name_corrected"] = df["name_corrected"].fillna(df["name"])
     
     df = df[["url_corrected","charity_numbers_corrected","summary_corrected","phone_corrected","email_corrected","locations_corrected","name_corrected"]]
-    print(df)
-    #conn.close()
+   
+    for row in df.itertuples():
+      #accessors.insert_charity(conn, row.url_corrected, row.name_corrected, row.summary_corrected)
+      #print(row.charity_numbers_corrected)
+      try:
+         charity_nums = ast.literal_eval(row.charity_numbers_corrected)
+         #for c in charity_nums:
+         #   if charity_nums[c] is not None:
+         #      accessors.insert_charity_number(conn, row.url_corrected, charity_nums[c], c)
+      except Exception as e:
+         print(f"Error: {e}")
+      
+      print(row.phone_corrected)
+
+ #   conn.close()
