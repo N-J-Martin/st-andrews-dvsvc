0833a3c01b121e6e6185aef56defdbc9b12a2a4e
Nadine Martin <nm275@st-andrews.ac.uk>
2025-07-22 15:07:16 +0100

    script to merge corrections from csv file - should be joined later used in llm_output_db

---
+++ scripts/compile_corrections.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# link to llm output DB at some point
+IN_FILE = "../resource/extracted_data_service_context(in).csv"
+OUT_FILE = "../resource/expected_output.csv"
+
+df = pd.read_csv(IN_FILE, usecols=["url", "charity_numbers", "summary", "charity_name", "services", "charity_numbers_corrected","summary_corrected","services_corrected", "charity_name_corrected", "url_corrected"] )
+df["url_corrected"] = df["url_corrected"].fillna(df["url"])
+df["charity_numbers_corrected"] = df["charity_numbers_corrected"].fillna(df["charity_numbers"])
+df["summary_corrected"] = df["summary_corrected"].fillna(df["summary"])
+df["services_corrected"] = df["services_corrected"].fillna(df["services"])
+df["charity_name_corrected"] = df["charity_name_corrected"].fillna(df["charity_name"])
+
+df = df[["charity_numbers_corrected","summary_corrected","services_corrected", "charity_name_corrected", "url_corrected"]]
+
+df.to_csv(OUT_FILE, index=False)
+print("written to" + OUT_FILE)
