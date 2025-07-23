e8b6060f4b55d36fc8d671e56340fe0215f0cbdc
Nadine Martin <nm275@st-andrews.ac.uk>
2025-07-23 18:51:04 +0100

    tidying up both scripts

--- scripts/compare_output.py
+++ scripts/compare_output.py
@@ -4,60 +4,73 @@ import ast
 EXPECTED_FILE = "../resource/expected_output.csv"
 IN_FILE = "../resource/extracted_data_service_context(in).csv"
 
-correct = 0
-# based on url - as none were corrected
-phone_dict = {}
-email_dict = {}
-charity_dict = {}
+def get_expected_results(file: str) -> tuple[dict, dict, dict]:
+    # based on url - as none were corrected
+    phone_dict = {}
+    email_dict = {}
+    charity_dict = {}
 
-# reads file of expected output.and sets up dictionaries linking url to emails, phone numbers and charity numbers
-expected = pd.read_csv(EXPECTED_FILE, usecols=["url_corrected", "charity_numbers_corrected","summary_corrected","services_corrected", "charity_name_corrected"] )
+    # reads file of expected output.and sets up dictionaries linking url to emails, phone numbers and charity numbers
+    expected = pd.read_csv(file, usecols=["url_corrected", "charity_numbers_corrected","summary_corrected","services_corrected", "charity_name_corrected"] )
 
-for i, d in expected.iterrows():
-    service_list = ast.literal_eval(d["services_corrected"])
-    phones = set()
-    emails = set()
-    for s in service_list:
-        if 'phone' in s and s['phone']:
-            phone = s['phone'].split(",")
-            phones.update(phone)
+    for i, d in expected.iterrows():
+        service_list = ast.literal_eval(d["services_corrected"])
+        phones = set()
+        emails = set()
+        for s in service_list:
+            if 'phone' in s and s['phone']:
+                phone = s['phone'].split(",")
+                phones.update(phone)
+            
+            if 'email' in s and s['email']:
+                email = s['email'].split(",")
+                emails.update(email)
+
+        phone_dict[d["url_corrected"]] = phones.copy()
+        email_dict[d["url_corrected"]] = emails.copy()
+
         
-        if 'email' in s and s['email']:
-            email = s['email'].split(",")
-            emails.update(email)
+        expected_charity_nums = ast.literal_eval(d["charity_numbers_corrected"])
+        expected_charity_nums = list(expected_charity_nums.values())
+        charity_dict[d["url_corrected"]] = expected_charity_nums
 
-    phone_dict[d["url_corrected"]] = phones.copy()
-    email_dict[d["url_corrected"]] = emails.copy()
-
-    
-    expected_charity_nums = ast.literal_eval(d["charity_numbers_corrected"])
-    expected_charity_nums = list(expected_charity_nums.values())
-    charity_dict[d["url_corrected"]] = expected_charity_nums
+    return phone_dict, email_dict, charity_dict
 
 
 # reads response file, collects et of phone numbers, email addresses and charity numbers to compare with expected.
+def count_correct_responses(file: str, phone_dict: dict, email_dict: dict, charity_dict: dict ) -> float:
+    correct = 0
+    response = pd.read_csv(file, usecols=["url", "charity_numbers","summary","services", "charity_name"] )
 
-response = pd.read_csv(IN_FILE, usecols=["url", "charity_numbers","summary","services", "charity_name"] )
+    for i, d in response.iterrows():
+        service_list = ast.literal_eval(d["services"])
+        phones = set()
+        emails = set()
 
-for i, d in response.iterrows():
-    service_list = ast.literal_eval(d["services"])
-    phones = set()
-    emails = set()
+        for s in service_list:
+            if 'phone' in s and s['phone']:
+                phone = s['phone'].split(",")
+                phones.update(phone)
 
-    for s in service_list:
-        if 'phone' in s and s['phone']:
-            phone = s['phone'].split(",")
-            phones.update(phone)
+            if 'email' in s and s['email']:
+                email = s['email'].split(",")
+                emails.update(email)
 
-        if 'email' in s and s['email']:
-            email = s['email'].split(",")
-            emails.update(email)
+        charity_nums = ast.literal_eval(d["charity_numbers"])
+        charity_nums = list(charity_nums.values())
 
-    charity_nums = ast.literal_eval(d["charity_numbers"])
-    charity_nums = list(charity_nums.values())
+    
+        if phone_dict[d["url"]] == phones and email_dict[d["url"]] == emails and charity_dict[d["url"]] == charity_nums:
+            correct += 1
 
-    if phone_dict[d["url"]] == phones and email_dict[d["url"]] == emails and charity_dict[d["url"]] == charity_nums:
-        correct += 1
+    # return percentage of correct responses at the end
+    return (correct / len(phone_dict))*100
 
-# outputs percentage of correct responses (all of phones numbers, emails, and charity numbers match), to 3 significant figures
-print(f"Percentage of responses correct (3 S.F): {(correct / len(phone_dict))*100:.3}")
+def main():
+    phone_dict, email_dict, charity_dict = get_expected_results(EXPECTED_FILE)
+    # outputs percentage of correct responses (all of phones numbers, emails, and charity numbers match), to 3 significant figures
+    print(f"Percentage of responses correct (3 S.F): {count_correct_responses(IN_FILE, phone_dict, email_dict, charity_dict):.3}")
+
+if __name__ == "__main__":
+    main()
+
--- scripts/compile_corrections.py
+++ scripts/compile_corrections.py
@@ -2,16 +2,26 @@ import pandas as pd
 
 # link to llm output DB at some point
 IN_FILE = "../resource/extracted_data_service_context(in).csv"
-OUT_FILE = "../resource/blahoutput.csv"
+OUT_FILE = "../resource/output.csv"
 
-df = pd.read_csv(IN_FILE, usecols=["url", "charity_numbers", "summary", "charity_name", "services", "charity_numbers_corrected","summary_corrected","services_corrected", "charity_name_corrected", "url_corrected"] )
-df["url_corrected"] = df["url_corrected"].fillna(df["url"])
-df["charity_numbers_corrected"] = df["charity_numbers_corrected"].fillna(df["charity_numbers"])
-df["summary_corrected"] = df["summary_corrected"].fillna(df["summary"])
-df["services_corrected"] = df["services_corrected"].fillna(df["services"])
-df["charity_name_corrected"] = df["charity_name_corrected"].fillna(df["charity_name"])
+def merge(file: str):
+    df = pd.read_csv(IN_FILE, usecols=["url", "charity_numbers", "summary", "charity_name", "services", "charity_numbers_corrected","summary_corrected","services_corrected", "charity_name_corrected", "url_corrected"] )
+    df["url_corrected"] = df["url_corrected"].fillna(df["url"])
+    df["charity_numbers_corrected"] = df["charity_numbers_corrected"].fillna(df["charity_numbers"])
+    df["summary_corrected"] = df["summary_corrected"].fillna(df["summary"])
+    df["services_corrected"] = df["services_corrected"].fillna(df["services"])
+    df["charity_name_corrected"] = df["charity_name_corrected"].fillna(df["charity_name"])
+    df = df[["charity_numbers_corrected","summary_corrected","services_corrected", "charity_name_corrected", "url_corrected"]]
 
-df = df[["charity_numbers_corrected","summary_corrected","services_corrected", "charity_name_corrected", "url_corrected"]]
+    return df
 
-df.to_csv(OUT_FILE, index=False)
-print("written to" + OUT_FILE)
+
+
+def main():
+    df = merge(IN_FILE)
+    df.to_csv(OUT_FILE, index=False)
+    print("Corrections written to" + OUT_FILE)
+
+
+if __name__ == "__main__":
+    main()
