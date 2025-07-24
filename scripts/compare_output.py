4f0b0f184729e482ccdbeb6d03747662e011986a
Nadine Martin <nm275@st-andrews.ac.uk>
2025-07-24 16:48:10 +0100

    interactive mode

--- scripts/compare_output.py
+++ scripts/compare_output.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import ast
 import phonenumbers
+import getopt, sys
 
 EXPECTED_FILE = "../resource/expected_output.csv"
-IN_FILE = "../resource/extracted-24-07-25-14-25.csv"
+IN_FILE = "../resource/extracted-24-07-25-15-55.csv"
 
 def standardise_phone_number(x):
    try:
@@ -50,7 +51,7 @@ def get_expected_results(file: str) -> tuple[dict, dict, dict]:
 
 
 # reads response file, collects et of phone numbers, email addresses and charity numbers to compare with expected.
-def count_correct_responses(file: str, phone_dict: dict, email_dict: dict, charity_dict: dict ) -> float:
+def count_correct_responses(file: str, phone_dict: dict, email_dict: dict, charity_dict: dict, interactive: bool ) -> float:
     correct = 0
     counted = 0
     response = pd.read_csv(file, usecols=["url", "charity_numbers","summary","services", "charity_name"] )
@@ -78,30 +79,51 @@ def count_correct_responses(file: str, phone_dict: dict, email_dict: dict, chari
             charity_nums = list(charity_nums.values())
             charity_nums = list(filter(lambda x: x != None and x != "", charity_nums))
 
-        
+            passed = False
             if phone_dict[d["url"]] != phones:
                 print(f"Expected: {phone_dict[d["url"]]}")
                 print(f"Actual: {phones}")
-                print("============")
+                print()
             elif email_dict[d["url"]] != emails:
                 print(f"Expected: {email_dict[d["url"]]}")
                 print(f"Actual: {emails}")
-                print("============")
+                print()
             elif charity_dict[d["url"]] != charity_nums:
                 print(f"Expected: {charity_dict[d["url"]]}")
                 print(f"Actual: {charity_nums}")
-                print("============")
+                print()
             else:
                 correct += 1
-        
+                passed = True
+
+            # interactive provides option to manually mark responses as correct
+            if not passed and interactive:
+                override = input(f"Override {d["url"]}? (Y/N)")
+                if override.upper()[0] == 'Y':
+                    correct+=1
+                print("============")
 
     # return percentage of correct responses at the end
     return (correct / counted)*100
 
 def main():
+    interactive = False
+    argList  = sys.argv[1:]
+    options = "i"
+    long_options = "interactive"
+    # use -i or --interactive as command line argument to use itneractive mode to manually correct responses that don't match.    
+    try:
+        args, vals = getopt.getopt(argList, options, long_options)
+        for a, v in args:
+            if a in ("-i", "--interactive"):
+                interactive = True
+
+    except getopt.error as err:
+        print (str(err))
+    
     phone_dict, email_dict, charity_dict = get_expected_results(EXPECTED_FILE)
     # outputs percentage of correct responses (all of phones numbers, emails, and charity numbers match), to 3 significant figures
-    print(f"Percentage of responses correct (3 S.F): {count_correct_responses(IN_FILE, phone_dict, email_dict, charity_dict):.3}")
+    print(f"Percentage of responses correct (3 S.F): {count_correct_responses(IN_FILE, phone_dict, email_dict, charity_dict, interactive):.3}")
 
 if __name__ == "__main__":
     main()
