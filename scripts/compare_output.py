e01dbd91a6c64bb316fade42802f5bef2bbbb5c8
Nadine Martin <nm275@st-andrews.ac.uk>
2025-07-24 15:06:08 +0100

    ensure only comparing urls in the expected output file

--- scripts/compare_output.py
+++ scripts/compare_output.py
@@ -2,7 +2,7 @@ import pandas as pd
 import ast
 
 EXPECTED_FILE = "../resource/expected_output.csv"
-IN_FILE = "../resource/extracted_data_service_context(in).csv"
+IN_FILE = "../resource/extracted-24-07-25-14-25.csv"
 
 def get_expected_results(file: str) -> tuple[dict, dict, dict]:
     # based on url - as none were corrected
@@ -43,25 +43,27 @@ def count_correct_responses(file: str, phone_dict: dict, email_dict: dict, chari
     response = pd.read_csv(file, usecols=["url", "charity_numbers","summary","services", "charity_name"] )
 
     for i, d in response.iterrows():
-        service_list = ast.literal_eval(d["services"])
-        phones = set()
-        emails = set()
+        # only checks urls in expected output
+        if d["url"] in phone_dict: 
+            service_list = ast.literal_eval(d["services"])
+            phones = set()
+            emails = set()
 
-        for s in service_list:
-            if 'phone' in s and s['phone']:
-                phone = s['phone'].split(",")
-                phones.update(phone)
+            for s in service_list:
+                if 'phone' in s and s['phone']:
+                    phone = s['phone'].split(",")
+                    phones.update(phone)
 
-            if 'email' in s and s['email']:
-                email = s['email'].split(",")
-                emails.update(email)
+                if 'email' in s and s['email']:
+                    email = s['email'].split(",")
+                    emails.update(email)
 
-        charity_nums = ast.literal_eval(d["charity_numbers"])
-        charity_nums = list(charity_nums.values())
+            charity_nums = ast.literal_eval(d["charity_numbers"])
+            charity_nums = list(charity_nums.values())
 
-    
-        if phone_dict[d["url"]] == phones and email_dict[d["url"]] == emails and charity_dict[d["url"]] == charity_nums:
-            correct += 1
+        
+            if phone_dict[d["url"]] == phones and email_dict[d["url"]] == emails and charity_dict[d["url"]] == charity_nums:
+                correct += 1
 
     # return percentage of correct responses at the end
     return (correct / len(phone_dict))*100
