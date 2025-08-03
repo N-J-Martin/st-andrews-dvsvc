990701a6fe2ba7609e7bbce667ad97783a267d3d
Nadine Martin <nm275@st-andrews.ac.uk>
2025-08-03 16:52:23 +0100

    Ensure is clear what code is doing

--- scripts/compare_output.py
+++ scripts/compare_output.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import ast
-import re
 import phonenumbers
-import argparse, sys
+import argparse
 
 EXPECTED_FILE = "../resource/expected_output.csv"
 IN_FILE = "extracted_data.csv"
 
-
+# reformats phone numbers into standard E164 format
 def standardise_phone_number(x):
    try:
       return phonenumbers.format_number(phonenumbers.parse(str(x), "GB"), phonenumbers.PhoneNumberFormat.E164)
    except:
       return None
 
+# Creates dictionaries linking charity urls to expected phone numbers, emails and charity numbers
 def get_expected_results(file: str) -> tuple[dict, dict, dict]:
     # based on url - as none were corrected
     phone_dict = {}
@@ -50,6 +50,8 @@ def get_expected_results(file: str) -> tuple[dict, dict, dict]:
 
     return phone_dict, email_dict, charity_dict
 
+# formats llm responses into dictionaries for phone numbers, emails and charity numbers, with the urls as keys
+# essentially the same as get_expected_results
 def get_response_details(file: str) -> tuple[dict, dict, dict]:
     phone_dict = {}
     email_dict = {}
@@ -98,7 +100,7 @@ def get_response_details(file: str) -> tuple[dict, dict, dict]:
 
 
 
-# reads response file, collects et of phone numbers, email addresses and charity numbers to compare with expected.
+# compares of phone numbers, email addresses and charity numbers of the llm responses with their expected values.
 def count_correct_responses(exp_phone_dict: dict, exp_email_dict: dict, exp_charity_dict: dict, new_phone_dict: dict, new_email_dict: dict, new_charity_dict: dict,  interactive: bool, log: bool ) -> float:
     correct = 0
     counted = 0
@@ -138,6 +140,7 @@ def count_correct_responses(exp_phone_dict: dict, exp_email_dict: dict, exp_char
     # return percentage of correct responses at the end
     return (correct / counted)*100
 
+# extracts the text used in the llm prompts, relate back to ur
 def get_paragraph_text(file: str) -> dict:
     para_dict = {}
     response = pd.read_csv(file, usecols=["url", "paragraph_text"])
@@ -145,53 +148,54 @@ def get_paragraph_text(file: str) -> dict:
         para_dict[d["url"]] = d["paragraph_text"]
     return para_dict
 
+# identifies whether all values in a list are featured in the paragraph text
 def check_value_on_page(url:str, targets: list, paragraphs: str,interactive: bool,  log: bool) -> bool:
     for value in targets:
         if value is not None and value not in paragraphs:
             if log or interactive:
                 print(f"Could not find '{value}' in {url}.\n")
+                # interactive allows overriding of false negatives
                 if interactive:
                     override = input(f"Override {url}? (Y/N)")
                     print("============")
                     if override.upper()[0] != 'Y':
                         return False
                    
-
-                else:
-                    return False
+            else:
+                return False
     return True
     
+# computes percentage of llm responses held values found in the text given in the prompt
 def check_details_on_page(phone_dict: dict, email_dict: dict, charity_dict: dict, page_dict: dict, interactive: bool, log:bool) -> float:
     correct = 0
-    count = 0 # 1 nan pages
+    count = 0 # 1 nan page
     for url, page in page_dict.items():
         if not pd.isnull(page):
             count += 1
             if check_value_on_page(url, phone_dict[url], page, interactive, log) and check_value_on_page(url, email_dict[url], page, interactive,  log) and check_value_on_page(url, charity_dict[url], page, interactive, log):
                 correct += 1
-
+    print(correct)
     return (correct/count)*100
 
 
 
 def main():
-    interactive = False
-    # use -i or --interactive as command line argument to use itneractive mode to manually correct responses that don't match.    
+    # flags for interactive (manually correct false negatives), the mode - comparing against expected, or comparing against page text, produce logs of mismatches 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interactive", action='store_true')
-    parser.add_argument("-m", "--mode", required=True, choices=["expected", "inplace"])
-    parser.add_argument("-l", "--logs", action='store_true')
+    parser.add_argument("-i", "--interactive", action='store_true', help="manually correct false negatives")
+    parser.add_argument("-m", "--mode", required=True, choices=["expected", "inplace"], help="comparing against expected (expected), or comparing against page text (inplace)")
+    parser.add_argument("-l", "--logs", action='store_true', help="outputs any mismatches")
     args = parser.parse_args()
     
     new_phone_dict, new_email_dict, new_charity_dict = get_response_details(IN_FILE)
     if args.mode == "expected":
         phone_dict, email_dict, charity_dict = get_expected_results(EXPECTED_FILE)
         result = count_correct_responses(phone_dict, email_dict, charity_dict, new_phone_dict, new_email_dict, new_charity_dict, args.interactive, args.logs)
-        # outputs percentage of correct responses (all of phones numbers, emails, and charity numbers match), to 3 significant figures
         print(f"Percentage of responses correct (3 S.F): {result:.3}")
 
     else:
         result = check_details_on_page(new_phone_dict, new_email_dict, new_charity_dict, get_paragraph_text(IN_FILE), args.interactive, args.logs)
+        print(result)
         print(f"Percentage of responses whose values can be found in input (3 S.F): {result:.3}")
 
 if __name__ == "__main__":
