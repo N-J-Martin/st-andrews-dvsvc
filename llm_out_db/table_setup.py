d034e001ad46c7c995390ed47c3c42362ddaf8f4
Nadine Martin <nm275@st-andrews.ac.uk>
2026-02-11 21:04:41 +0000

    removed magic numbers

--- llm_out_db/table_setup.py
+++ llm_out_db/table_setup.py
@@ -1,16 +1,18 @@
 import psycopg2
 from llm_setup import get_db_logger, connect
-
+CHARITY_NUM_LENGTH = 8
+MAX_STR_LENGTH = 2048 
+PHONE_LENGTH = 30
 
 def create_charity_table( conn: psycopg2.extensions.connection):
     # url regex from https://www.freecodecamp.org/news/how-to-write-a-regular-expression-for-a-url/
     with conn.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(f"""
         DROP TABLE IF EXISTS charity CASCADE;
         CREATE TABLE charity(
-        url VARCHAR(2048) PRIMARY KEY,
-        name VARCHAR (2048) NOT NULL,
-        summary VARCHAR(2048),
+        url VARCHAR({MAX_STR_LENGTH}) PRIMARY KEY,
+        name VARCHAR ({MAX_STR_LENGTH}) NOT NULL,
+        summary VARCHAR({MAX_STR_LENGTH}),
         CHECK ( url ~ '(https://www.|http://www.|https://|http://)?[a-zA-Z]{2,}(.[a-zA-Z]{2,})(.[a-zA-Z]{2,})?/[a-zA-Z0-9]{2,}|((https://www.|http://www.|https://|http://)?[a-zA-Z]{2,}(.[a-zA-Z]{2,})(.[a-zA-Z]{2,})?)|(https://www.|http://www.|https://|http://)?[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,}(.[a-zA-Z0-9]{2,})?')
         );""")
 
@@ -20,12 +22,12 @@ def create_charity_table( conn: psycopg2.extensions.connection):
 
 def create_service_table(conn: psycopg2.extensions.connection):
     with conn.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(f"""
             DROP TABLE IF EXISTS service CASCADE;
             CREATE TABLE service(
-                url VARCHAR(2048) REFERENCES charity(url),
+                url VARCHAR({MAX_STR_LENGTH}) REFERENCES charity(url),
                 service_id INT,
-                description VARCHAR(2048),
+                description VARCHAR({MAX_STR_LENGTH}),
                 PRIMARY KEY (url, service_id)
             );
                        """)
@@ -38,16 +40,16 @@ def create_service_table(conn: psycopg2.extensions.connection):
 def create_charity_num_table( conn: psycopg2.extensions.connection):
     # improve charity number regex - current 1-8 alphanumeric digits
     with conn.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(f"""
         DROP TABLE IF EXISTS charity_num CASCADE;
         CREATE TABLE charity_num(
-        url VARCHAR(2048) REFERENCES charity(url),
-        charity_number VARCHAR(8) NOT NULL,
-        government varchar(2048) NOT NULL,
+        url VARCHAR({MAX_STR_LENGTH}) REFERENCES charity(url),
+        charity_number VARCHAR({CHARITY_NUM_LENGTH}) NOT NULL,
+        government varchar({MAX_STR_LENGTH}) NOT NULL,
         PRIMARY KEY (url, charity_number),
         CHECK ( url ~ '(https://www.|http://www.|https://|http://)?[a-zA-Z]{2,}(.[a-zA-Z]{2,})(.[a-zA-Z]{2,})?/[a-zA-Z0-9]{2,}|((https://www.|http://www.|https://|http://)?[a-zA-Z]{2,}(.[a-zA-Z]{2,})(.[a-zA-Z]{2,})?)|(https://www.|http://www.|https://|http://)?[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,}(.[a-zA-Z0-9]{2,})?'),
         CHECK (government = 'england_wales' OR government = 'scotland' OR government = 'northern_ireland'),
-        CHECK (charity_number ~ '[a-zA-z0-9]{1,8}')
+        CHECK (charity_number ~ '[a-zA-z0-9]{1,CHARITY_NUM_LENGTH}')
         );""")
 
         conn.commit()
@@ -57,12 +59,12 @@ def create_charity_num_table( conn: psycopg2.extensions.connection):
 def create_phone_num_table( conn: psycopg2.extensions.connection):
     # using E164 phone number format as is international standard
     with conn.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(f"""
         DROP TABLE IF EXISTS phone_num CASCADE;
         CREATE TABLE phone_num(
-         url VARCHAR(2048),
+         url VARCHAR({MAX_STR_LENGTH}),
          service_id INT,    
-         phone_number VARCHAR(30),
+         phone_number VARCHAR({PHONE_LENGTH}),
          FOREIGN KEY (url, service_id) REFERENCES service(url, service_id),
          PRIMARY KEY(url, service_id, phone_number),
          CHECK ( url ~ '(https://www.|http://www.|https://|http://)?[a-zA-Z]{2,}(.[a-zA-Z]{2,})(.[a-zA-Z]{2,})?/[a-zA-Z0-9]{2,}|((https://www.|http://www.|https://|http://)?[a-zA-Z]{2,}(.[a-zA-Z]{2,})(.[a-zA-Z]{2,})?)|(https://www.|http://www.|https://|http://)?[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,}(.[a-zA-Z0-9]{2,})?'),
@@ -76,12 +78,12 @@ def create_phone_num_table( conn: psycopg2.extensions.connection):
 def create_email_table( conn: psycopg2.extensions.connection):
     # email regex from https://www.geeksforgeeks.org/how-to-validate-email-address-using-regexp-in-javascript/
     with conn.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(f"""
         DROP TABLE IF EXISTS email CASCADE;
         CREATE TABLE email(
-         url VARCHAR(2048),
+         url VARCHAR({MAX_STR_LENGTH}),
          service_id INT,
-         email VARCHAR(2048),
+         email VARCHAR({MAX_STR_LENGTH}),
          FOREIGN KEY (url, service_id) REFERENCES service(url, service_id),
          PRIMARY KEY(url, service_id, email),
          CHECK ( url ~ '(https://www.|http://www.|https://|http://)?[a-zA-Z]{2,}(.[a-zA-Z]{2,})(.[a-zA-Z]{2,})?/[a-zA-Z0-9]{2,}|((https://www.|http://www.|https://|http://)?[a-zA-Z]{2,}(.[a-zA-Z]{2,})(.[a-zA-Z]{2,})?)|(https://www.|http://www.|https://|http://)?[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,}(.[a-zA-Z0-9]{2,})?'),
@@ -94,11 +96,11 @@ def create_email_table( conn: psycopg2.extensions.connection):
 
 def create_location_table( conn: psycopg2.extensions.connection):
     with conn.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(f"""
         DROP TABLE IF EXISTS location CASCADE;
         CREATE TABLE location(
          id INT PRIMARY KEY ,
-         name VARCHAR(2048)
+         name VARCHAR({MAX_STR_LENGTH})
         );""")
 
         conn.commit()
@@ -107,10 +109,10 @@ def create_location_table( conn: psycopg2.extensions.connection):
 
 def create_service_location_table( conn: psycopg2.extensions.connection):
     with conn.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(f"""
         DROP TABLE IF EXISTS service_location CASCADE;
         CREATE TABLE service_location(
-          url VARCHAR(2048),
+          url VARCHAR({MAX_STR_LENGTH}),
           service_id INT,
           id INT REFERENCES location(id),
           FOREIGN KEY (url, service_id) REFERENCES service(url, service_id),
