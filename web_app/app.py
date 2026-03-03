ece5bb5c704109174a07beecf1cef4d726870c12
Nadine Martin <nm275@st-andrews.ac.uk>
2026-03-03 21:32:48 +0000

    improved formatting

--- web_app/app.py
+++ web_app/app.py
@@ -9,7 +9,22 @@ def location_filter():
         nearby = queries.getLocations(current_loc, int(current_dist))
         outstr = ""
         for l in nearby:
-            outstr = outstr + f"<p>{l}</p>"
+            outstr = outstr + f"""<div style="outline: thick inset">
+            <h3>{l[0]}</h3>
+            <a href='{l[1]}'> {l[1]} </a>
+            <br>
+            <br>
+            location: {l[3]}
+            <br>
+            phone number: {l[4]}
+            <br>
+            email: {l[5]}
+            <br>
+            <br>
+            description: {l[2]}
+            <br>
+
+            </div>"""
 
         if nearby == []:
             return "<h1>Error searching location</h1>"
