c03c02b310859dedb26d0d6bf6d7ede36d47e735
Nadine Martin <nm275@st-andrews.ac.uk>
2026-05-26 19:30:46 +0100

    paginate normal query too

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 import queries
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
@@ -28,6 +28,7 @@ def location_filter():
     if request.method == "POST":
         current_loc = request.form["loc"]
         current_dist = int(request.form["dist"])
+        original_dist = current_dist
         address, lat, long = get_coordinates(current_loc)
         if address is None:
             nearby = []
@@ -42,32 +43,15 @@ def location_filter():
         
         # distance should be 1000, so just search for all with UK charities
         if nearby == []:
-           return redirect(url_for("all_charity_list", page=1))
+           return redirect(url_for("all_charity_list",location = current_loc, distance=original_dist, page=1))
         else:
-            outstr = f""" {'<p>All charities with UK locations</p>' if current_dist > UK_LENGTH else '<p>All charities within ' + str(current_dist) + ' km of ' + address + '</p>'}<ul>"""
+           return redirect(url_for("search_charity_list",location = current_loc, distance=original_dist, current_dist = current_dist, page=1))
 
-       
-            for l in nearby:
-                outstr = outstr + f"""<li onclick="location.href='{url_for('charity_page',index=l[0])}';", style="outline: thick inset"><div >
-                <h3>{l[1]}</h3>
-                <a href='{l[3]}'> {l[3]} </a>
-                <br>
-                <br>
-                location: {l[2]}, distance: {l[5]/1000}
-                <br>
-                <br>
-                summary: {l[4]}
-                <br>
-
-                </div></li>"""
-
-
-        return render_template("index.html", result=outstr+"</ul>")
-
-    return render_template("index.html")
+    return render_template("index.html", loc="location", dist="distance")
 
 @app.route("/all/<page>")
-def all_charity_list(page):
+@app.route("/all/<location>/<distance>/<page>")
+def all_charity_list(page, location="location", distance="distance", current_dist=0): #current_dist to match search_charity_list
     charity_list = queries.get_all_charities()
     outstr = f"""<p>Could not find Location. All charities in database</p> <ul>"""
     page = int(page)
@@ -86,16 +70,48 @@ def all_charity_list(page):
     num_pages = int(len(charity_list)/PAGE_LIMIT)
     if (len(charity_list) % PAGE_LIMIT != 0):
         num_pages += 1
-    outstr = outstr + page_links(num_pages, page, "all_charity_list")
-    return render_template("index.html", result=outstr)
+    outstr = outstr + page_links(num_pages, page, "all_charity_list", location, distance, current_dist)
+    return render_template("index.html", result=outstr, loc=location, dist=distance)
 
-def page_links(num_pages, curr_page, redirect):
+@app.route("/<location>/<distance>/<current_dist>/<page>")
+def search_charity_list(page, location, distance, current_dist):
+    address, lat, long = get_coordinates(location)
+    if address is None:
+        charity_list = []
+    else:
+        charity_list = queries.get_locations(lat, long, int(current_dist))
+    outstr = f""" {'<p>All charities with UK locations</p>' if int(current_dist) > UK_LENGTH else '<p>All charities within ' + str(current_dist) + ' km of ' + address + '</p>'}<ul>"""  
+
+    page = int(page)
+    for c in charity_list[(page-1)*PAGE_LIMIT: (page)*PAGE_LIMIT]:
+        outstr = outstr + f"""<li onclick="location.href='{url_for('charity_page',index=c[0])}';", style="outline: thick inset"><div >
+                <h3>{c[1]}</h3>
+                <a href='{c[3]}'> {c[3]} </a>
+                <br>
+                <br>
+                location: {c[2]}, distance: {c[5]/1000}
+                <br>
+                <br>
+                summary: {c[4]}
+                <br>
+
+                </div></li>"""
+
+    outstr += "</ul>"
+    num_pages = int(len(charity_list)/PAGE_LIMIT)
+    if (len(charity_list) % PAGE_LIMIT != 0):
+        num_pages += 1
+    outstr = outstr + page_links(num_pages, page, "search_charity_list", location, distance, current_dist)
+    return render_template("index.html", result=outstr, loc=location, dist=distance)
+
+
+def page_links(num_pages, curr_page, redirect, location, distance, current_dist):
     out = "<div> <ul class='pagination'>"
     for i in range(1, num_pages+1):
         if i == curr_page:
             out = out + f"""<li><u>{i}</u></li>"""
         else:
-            out = out + f"""<li><a href={url_for(redirect, page=i)}>{i}</a></li>"""
+            out = out + f"""<li><a href={url_for(redirect, page=i, location=location, distance=distance, current_dist=current_dist)}>{i}</a></li>"""
     out += "</ul> </div>"
     return out
 
