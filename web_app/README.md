c7563ce8ec19d544321e656729a3ede60d0906e4
Nadine Martin <nm275@st-andrews.ac.uk>
2026-05-28 11:41:55 +0100

    improved documentation

---
+++ web_app/README.md
@@ -0,0 +1,31 @@
+# DASHED Web App
+This is the web front end for the automated domestic violence support database. 
+It is built using Flask, and additionally uses Neomatim (https://nominatim.org/) via GeoPy (https://geopy.readthedocs.io/en/stable/), and the POSTGRESQL database managed by directory `llm_out_db`.
+
+## Current Features
+Enter postcode and a distance (between 0 and 1000km) to search for domestic violence support organisations that have a service within that area, ordered by distance of service. Organisations are repeated for each service. 
+
+The search distance will expand until either a charity is found, or the distance exceeds 1000km.
+
+If the location entered is not valid (geocoder does not return coordinates for the value), then all charities in the database are shown. This can also be accessed by /all/1.
+
+Search results are paginated
+
+Click on a service in the search results to navigate to a page about that organisation, displaying general information about the charity and a list of services they provide.
+
+## Running Locally
+This requires `llm_out_db_db_1` to be running. This can be set up as described in the README in `llm_out_db` directory. You can restart the podman/docker container with `podman/docker compose restart llm_out_db_db_1`.
+
+Set up and activate a python virtual environment as described in the main README. Install the requirements for this directory, by navigating to this directory and running
+`pip install -r requirements.txt`.
+
+Then to host the web app locally, run `flask run`.
+The site can be accessed at `localhost:5000`
+
+## TODO
+- Show both charities with location in search radius, and nationwide examples (e.g: location is UK, England, Scotland, Wales, None).
+- Introduce a map to show locations
+- Filter by keywords in descriptions, leading to adjusting database backend and queries to allow filtering by other user requirements (accsssibility, service type, who they support etc).
+- Improve displah of information
+
+
--- web_app/app.py
+++ web_app/app.py
@@ -11,7 +11,9 @@ app = Flask(__name__)
 geolocator = Nominatim(user_agent=USER_AGENT)
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=DELAY)
 
-
+"""
+Retrieves coordinates of a given string location/postcode
+"""
 def get_coordinates(location: str):
     try:
       conv = geocode(f"{location}, UK", exactly_one=True)
@@ -22,7 +24,9 @@ def get_coordinates(location: str):
       print(f"Error: {e}")
       return None, None, None
 
-
+"""
+Takes in postcode and distance from form, and obtains list of charities in that distance, or nearest reasonable distance
+"""
 @app.route("/", methods = ['POST'])
 def location_filter():
     if request.method == "POST":
@@ -38,23 +42,25 @@ def location_filter():
             while nearby == [] and current_dist < UK_LENGTH:
                 nearby = queries.get_locations(lat, long, int(current_dist))
                 current_dist = current_dist + 50
-            
         
-        
-        # distance should be 1000, so just search for all with UK charities
+        # distance should be 1000 or not a location, so just search for all UK charities. Paginate search
         if nearby == []:
            return redirect(url_for("all_charity_list",location = current_loc, distance=original_dist, page=1))
         else:
            return redirect(url_for("search_charity_list",location = current_loc, distance=original_dist, current_dist = current_dist, page=1))
 
-    return render_template("index.html", loc="location", dist="distance")
+    return render_template("index.html", loc="Postcode", dist="distance")
 
+
+""" Paginates all uk search. Access separately, or through search"""
 @app.route("/all/<page>")
 @app.route("/all/<location>/<distance>/<page>")
 def all_charity_list(page, location="location", distance="distance", current_dist=0): #current_dist to match search_charity_list
+    # Have to requery database for each page - see if can avoid this, consider using cookies or session?
     charity_list = queries.get_all_charities()
     outstr = f"""<p>Could not find Location. All charities in database</p> <ul class='result'>"""
     page = int(page)
+    # list all charities within given range for that page, calculated from current length and the page limit (number of charities per page)
     for c in charity_list[(page-1)*PAGE_LIMIT: (page)*PAGE_LIMIT]:
         outstr = outstr + f"""<li onclick="location.href='{url_for('charity_page',index=c[0])}';", style="outline: thick inset"><div >
         <h3>{c[1]}</h3>
@@ -67,15 +73,19 @@ def all_charity_list(page, location="location", distance="distance", current_dis
         </div></li>"""
 
     outstr += "</ul>"
+
+    # build page navigation 
     num_pages = int(len(charity_list)/PAGE_LIMIT)
     if (len(charity_list) % PAGE_LIMIT != 0):
         num_pages += 1
     outstr = outstr + page_links(num_pages, page, "all_charity_list", location, distance, current_dist)
     return render_template("index.html", result=outstr, loc=location, dist=distance)
 
+""" Paginates search result for location and distance"""
 @app.route("/<location>/<distance>/<current_dist>/<page>")
 def search_charity_list(page, location, distance, current_dist):
     address, lat, long = get_coordinates(location)
+    # As in all_charity_list, have to requery database each time - limit to only values needed?
     if address is None:
         charity_list = []
     else:
@@ -83,6 +93,7 @@ def search_charity_list(page, location, distance, current_dist):
     outstr = f""" {'<p>All charities with UK locations</p>' if int(current_dist) > UK_LENGTH else '<p>All charities within ' + str(current_dist) + ' km of ' + address + '</p>'}<ul class='result'>"""  
 
     page = int(page)
+    # list all charities within given range for that page, calculated from current length and the page limit (number of charities per page)
     for c in charity_list[(page-1)*PAGE_LIMIT: (page)*PAGE_LIMIT]:
         outstr = outstr + f"""<li onclick="location.href='{url_for('charity_page',index=c[0])}';", style="outline: thick inset"><div >
                 <h3>{c[1]}</h3>
@@ -98,13 +109,16 @@ def search_charity_list(page, location, distance, current_dist):
                 </div></li>"""
 
     outstr += "</ul>"
+    # build page navigation 
     num_pages = int(len(charity_list)/PAGE_LIMIT)
     if (len(charity_list) % PAGE_LIMIT != 0):
         num_pages += 1
     outstr = outstr + page_links(num_pages, page, "search_charity_list", location, distance, current_dist)
     return render_template("index.html", result=outstr, loc=location, dist=distance)
 
-
+"""
+Page navigation at bottom of page, each button links to that number page
+"""
 def page_links(num_pages, curr_page, redirect, location, distance, current_dist):
     out = "<div> <ul class='pagination'>"
     for i in range(1, num_pages+1):
@@ -116,13 +130,17 @@ def page_links(num_pages, curr_page, redirect, location, distance, current_dist)
     return out
 
 
-
+"""
+Information about charity with that index number deisplayed in own page
+"""
 @app.route("/charity/<index>")
 def charity_page(index):
+    # link in stylesheet - currently builds from scratch here, use template instead?
     page = f"""<head><link rel="stylesheet" href="{ url_for('static', filename='index.css') }">
 </head>
 <body> 
 """
+    # get and display information about charity in general.
     charity_info = queries.get_charity_info_by_id(index)
     if charity_info == []:
         return "<h1>Unknown Charity</h1>"
@@ -139,9 +157,9 @@ def charity_page(index):
         Charity Number: {charity_info[2]}
     </div>
     """
-
+    # Get and display information about each service charity provides
     service_info = queries.get_all_service_info_by_charity_id(index)
-    service_details = "<h2>Services</h2><ul>"
+    service_details = "<h2>Services</h2><ul class=result>"
     for s in service_info:
         service_details = service_details + f"""
                 <li>
